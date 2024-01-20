2a86b6d3142cca572b5ae5029e07623426177458
camilo perez <cami20032017@gmail.com>
2024-01-20 11:36:05 -0500

    feat: adaptable desing

--- main.py
+++ main.py
@@ -5,13 +5,29 @@ import math
 root = Tk()
 root.title("Calculator")
 root.geometry("+500+80")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 
 styles = ttk.Style()
 styles.theme_use('clam')
 styles.configure('main_frame.TFrame', background="#DBDBDB")
 
 main_frame = ttk.Frame(root, style="main_frame.TFrame")
-main_frame.grid(column=0, row=0)
+main_frame.grid(column=0, row=0, sticky=(W, N, E, S))
+
+main_frame.columnconfigure(0, weight=1)
+main_frame.columnconfigure(1, weight=1)
+main_frame.columnconfigure(2, weight=1)
+main_frame.columnconfigure(3, weight=1)
+
+main_frame.rowconfigure(0, weight=1)
+main_frame.rowconfigure(1, weight=1)
+main_frame.rowconfigure(2, weight=1)
+main_frame.rowconfigure(3, weight=1)
+main_frame.rowconfigure(4, weight=1)
+main_frame.rowconfigure(5, weight=1)
+main_frame.rowconfigure(6, weight=1)
+main_frame.rowconfigure(7, weight=1)
 
 # styles label
 style_label_1 = ttk.Style()
@@ -22,11 +38,11 @@ style_label_2.configure('Label2.TLabel', font='arial 40', anchor="e")
 
 input_1 = StringVar()
 label_input_1 = ttk.Label(main_frame, textvariable=input_1, style='Label1.TLabel')
-label_input_1.grid(column=0, row=0, columnspan=4, sticky=(W, E))
+label_input_1.grid(column=0, row=0, columnspan=4, sticky=(W, N, E, S))
 
 input_2 = StringVar()
 label_input_2 = ttk.Label(main_frame, textvariable=input_2, style='Label2.TLabel')
-label_input_2.grid(column=0, row=1, columnspan=4, sticky=(W, E))
+label_input_2.grid(column=0, row=1, columnspan=4, sticky=(W, N, E, S))
 
 # styles button
 style_button_numbers = ttk.Style()
@@ -67,32 +83,32 @@ button_add = ttk.Button(main_frame, text="+", style='Button_remaining.TButton')
 button_equal = ttk.Button(main_frame, text="=", style='Button_remaining.TButton')
 button_square_root = ttk.Button(main_frame, text="√", style='Button_remaining.TButton')
 
-button_parenthesis_1.grid(column=0, row=2)
-button_parenthesis_2.grid(column=1, row=2)
-button_delete_all.grid(column=2, row=2)
-button_delete.grid(column=3, row=2)
+button_parenthesis_1.grid(column=0, row=2, sticky=(W, N, E, S))
+button_parenthesis_2.grid(column=1, row=2, sticky=(W, N, E, S))
+button_delete_all.grid(column=2, row=2, sticky=(W, N, E, S))
+button_delete.grid(column=3, row=2, sticky=(W, N, E, S))
 
-button_7.grid(column=0, row=3)
-button_8.grid(column=1, row=3)
-button_9.grid(column=2, row=3)
-button_divicion.grid(column=3, row=3)
+button_7.grid(column=0, row=3, sticky=(W, N, E, S))
+button_8.grid(column=1, row=3, sticky=(W, N, E, S))
+button_9.grid(column=2, row=3, sticky=(W, N, E, S))
+button_divicion.grid(column=3, row=3, sticky=(W, N, E, S))
 
-button_4.grid(column=0, row=4)
-button_5.grid(column=1, row=4)
-button_6.grid(column=2, row=4)
-button_multiplication.grid(column=3, row=4)
+button_4.grid(column=0, row=4, sticky=(W, N, E, S))
+button_5.grid(column=1, row=4, sticky=(W, N, E, S))
+button_6.grid(column=2, row=4, sticky=(W, N, E, S))
+button_multiplication.grid(column=3, row=4, sticky=(W, N, E, S))
 
-button_1.grid(column=0, row=5)
-button_2.grid(column=1, row=5)
-button_3.grid(column=2, row=5)
-button_add.grid(column=3, row=5)
+button_1.grid(column=0, row=5, sticky=(W, N, E, S))
+button_2.grid(column=1, row=5, sticky=(W, N, E, S))
+button_3.grid(column=2, row=5, sticky=(W, N, E, S))
+button_add.grid(column=3, row=5, sticky=(W, N, E, S))
 
-button_0.grid(column=0, row=6, columnspan=2, sticky=(W, E))
-button_point.grid(column=2, row=6)
-button_subtraction.grid(column=3, row=6)
+button_0.grid(column=0, row=6, columnspan=2, sticky=(W, N, E, S))
+button_point.grid(column=2, row=6, sticky=(W, N, E, S))
+button_subtraction.grid(column=3, row=6, sticky=(W, N, E, S))
 
-button_equal.grid(column=0, row=7, columnspan=3, sticky=(W, E))
-button_square_root.grid(column=3, row=7)
+button_equal.grid(column=0, row=7, columnspan=3, sticky=(W, N, E, S))
+button_square_root.grid(column=3, row=7, sticky=(W, N, E, S))
 
 for child in main_frame.winfo_children():
     child.grid_configure(ipady=10, padx=1, pady=1)
